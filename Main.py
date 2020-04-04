3850b867eb81bcfa225ea6063f9d6045a9b738ab
rag-hav <raghav.ag13@gmail.com>
2020-04-05 01:48:58 +0530

    First

---
+++ Main.py
@@ -0,0 +1,19 @@
+import praw
+import os
+
+#print((os.environ.get("praw_CLIENT_ID"),
+os.environ.get("praw_CLIENT_SECRET"),
+os.environ.get("praw_USER_AGENT"),
+os.environ.get("praw_USERNAME"),
+os.environ.get("praw_PASSWORD")))
+
+
+reddit = praw.Reddit(client_id=os.environ.get("praw_CLIENT_ID"),
+    client_secret=os.environ.get("praw_CLIENT_SECRET"),
+    user_agent=os.environ.get("praw_USER_AGENT"),
+    username=os.environ.get("praw_USERNAME"),
+    password=os.environ.get("praw_PASSWORD"))
+
+post_id='fueek3'
+submission=reddit.submission(post_id).reply("This is a not a tse from heroku s")
+
